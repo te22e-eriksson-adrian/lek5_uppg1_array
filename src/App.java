f6463445131b84ef2525a34f7d7b2e03513b322f
te22e-eriksson-adrian <adrian.eriksson@elev.ga.ntig.se>
2023-09-25 09:08:30 +0200

    uppg1 del a klar och fungerande

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int[] array1 = new int[5];
+        array1[0] = 8;
+        array1[1] = 4;
+        array1[2] = 6;
+        array1[3] = 3;
+        array1[4] = 9;
+
+        System.out.println("Talen i fältet: "+array1[0]+", "+array1[1]+", "+array1[2]+", "+array1[3]+", "+array1[4]);
+        double antal_siffror = array1.length;
+        System.out.println(antal_siffror);
+        double kvot = (array1[0]+array1[1]+array1[2]+array1[3]+array1[4])/antal_siffror;
+        System.out.println(kvot);
     }
 }
